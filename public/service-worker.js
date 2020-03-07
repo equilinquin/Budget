52dba2ea1490e5560395ea85e3c178c5d9e2513e
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-03-07 15:49:00 -0800

    added db.js file for offline changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,8 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
   ];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+  const CACHE_NAME = "static-cache-v3";
+  const DATA_CACHE_NAME = "data-cache-v2";
   
   // install
   self.addEventListener("install", function(evt) {
@@ -24,22 +24,22 @@ const FILES_TO_CACHE = [
   });
   
   // activate
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
+//   self.addEventListener("activate", function(evt) {
+//     evt.waitUntil(
+//       caches.keys().then(keyList => {
+//         return Promise.all(
+//           keyList.map(key => {
+//             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//               console.log("Removing old cache data", key);
+//               return caches.delete(key);
+//             }
+//           })
+//         );
+//       })
+//     );
   
-    self.clients.claim();
-  });
+//     self.clients.claim();
+//   });
   
   // fetch
   self.addEventListener("fetch", function(evt) {
@@ -52,7 +52,6 @@ const FILES_TO_CACHE = [
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
-  
               return response;
             })
             .catch(err => {
@@ -61,10 +60,8 @@ const FILES_TO_CACHE = [
             });
         }).catch(err => console.log(err))
       );
-  
       return;
     }
-  
     evt.respondWith(
       caches.open(CACHE_NAME).then(cache => {
         return cache.match(evt.request).then(response => {
